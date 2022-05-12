af6a87f5c6b5152a81423b401adf3b5c186307a4
sandeulju <tksemf1706@naver.com>
2022-05-12 18:07:55 +0900

    백준 1008번 node.js

--- 1/1008.js
+++ 1/1008.js
@@ -5,3 +5,31 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+let arr = [];
+
+rl.on("line", (x) => {
+    arr = x.split(' ');
+
+    const A = arr[0];
+    const B = arr[1];
+
+    const answer = A/B;
+    console.log(answer);
+
+    rl.close();
+});
+
+rl.on("close", () => {
+    process.exit();
+});
+
+// fs 시도
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+
+// const A = input[0];
+// const B = input[1];
+
+// const answer = A/B
+// console.log(answer)
+
