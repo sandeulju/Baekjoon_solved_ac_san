c0c110f807fc2714b715b030ac8176525f7725cd
sandeulju <tksemf1706@naver.com>
2022-05-21 21:50:10 +0900

    백준 10818 Node.js

--- 1/10818.js
+++ 1/10818.js
@@ -1,1 +1,20 @@
-const input=fs.readFileSync('/dev/stdin').toString().trim().split(/\s+/)
+const fs = require('fs');
+const input=fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0]);
+const numbers = input[1].split(' ').map(e => Number(e));
+
+let max = numbers[0];
+let min = numbers[0];
+
+for(let i=1; i<N; i++){
+    if(max < numbers[i]){
+        max = numbers[i];
+    }
+
+    if(min > numbers[i]){
+        min = numbers[i];
+    }
+}
+
+console.log(`${min} ${max}`)
